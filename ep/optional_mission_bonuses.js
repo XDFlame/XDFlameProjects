23dcaf70e226a36906ebb4c40a7a9e019dce946a
XDFlame <40347029+XDFlame@users.noreply.github.com>
2026-04-25 19:43:24 -0500

    Add files via upload

---
+++ ep/optional_mission_bonuses.js
@@ -0,0 +1,66 @@
+const optional_mission_bonuses = {
+	blacksite: {
+		no_alarms: true,
+		no_kills: true
+	},
+	financier: {
+		no_alarms: true,
+		no_kills: true
+	},
+	deposit: {
+		no_alarms: true,
+		no_kills: true
+	},
+	lakehouse: {
+		no_alarms: true,
+		no_kills: true
+	},
+	withdrawal: {
+		no_alarms: true,
+		no_kills: true
+	},
+	scientist: {
+		no_alarms: true,
+		no_kills: true
+	},
+	scrs: {
+		no_alarms: true,
+		no_kills: true,
+	},
+	black_dusk: {
+		no_alarms: true,
+		no_kills: false
+	},
+	killhouse: {
+		no_alarms: true,
+		no_kills: true
+	},
+	concept: {
+		no_alarms: false,
+		no_kills: true
+	},
+	setup: {
+		no_alarms: false,
+		no_kills: true
+	},
+	lockup: {
+		no_alarms: true,
+		no_kills: true
+	},
+	score: {
+		no_alarms: false,
+		no_kills: true
+	},
+	auction: {
+		no_alarms: false,
+		no_kills: true
+	},
+	gala: {
+		no_alarms: false,
+		no_kills: true
+	},
+	cache: {
+		no_alarms: false,
+		no_kills: true
+	}
+}
